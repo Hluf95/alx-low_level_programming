0329d196efd6518b5c1a8d80a2dd204ac82d70c1
Hluf95 <mlazy2118@gmail.com>
2022-11-22 02:57:37 -0800

    both projects

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,29 @@
+/*
+ * File:-memcpy.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *           to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
+ *
+ * Return: A pointer to the destination buffer @dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + j);
+		i += 1;
+		j += 1;
+	}
+	return (dest);
+}
